8a93d249928fcdba036b7b9ae42a8ce259fd76df
D. Rimron <darran@xalior.com>
2021-06-10 23:33:42 +0100

    Swap sc68 for binary package, since building it is a pain in the assets

--- packages/sc68/install.sh
+++ packages/sc68/install.sh
@@ -4,4 +4,4 @@
 cd src
 
 #Nothing grand, but allows the CI to wrap this operation to build custom packages for NextPi
-sudo make install
+sudo cp -rv usr /
--- packages/sc68/make.sh
+++ packages/sc68/make.sh
@@ -1,13 +1,8 @@
-#!/bin/sh
+#/bin/sh
+
 
 # Enter the source tree
 cd src
 
-# Bootstrap the (originally SVN) envrionment
-./tools/svn-bootstrap.sh
-
-# Configure the build chain, let it know we don't use /usr/local
-./configure --prefix=/usr
-
-# Now build
-make
+# THis is a binary package, extract the binary
+tar -zxvf sc68.tar.gz
